cd20e531cdf84342f84f04b11233b52446670ec6
chrissssss <ak_2001@gmx.de>
2025-03-16 07:30:59 +0000

    Stuff for debugging

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -3,6 +3,7 @@ package com.example.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Basic stub that's necessary for a Spring Boot application
 @SpringBootApplication
 public class HelloWorldApplication {
 
--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -1,13 +1,24 @@
 package com.example.helloworld;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+    Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
+    }
+
+    @PostMapping("/hello")
+    public String helloPost(@RequestBody String payload) {
+        logger.info("Received payload: " + payload);
+        return "You just called the POST method at '/hello'!\n";
     }
 }
